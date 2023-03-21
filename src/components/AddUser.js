ea1b23ce55cef9b0b96fb34822c04c361cef12e4
unknown <asha.engrbd@gmail.com>
2023-03-21 06:45:49 +0000

    client side to send data server side

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddUser = () => {
+
+    const [user, setUser] = useState({});
+    const handleAddUser = event =>{
+        event.preventDefault();
+        console.log(user);
+
+        fetch('http://localhost:5000/users',{
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
+
+    const handleInputBlur = event =>{
+        const field = event.target.name;
+        const value = event.target.value;
+        const newUser = {...user}
+        newUser[field] = value;
+        setUser(newUser);
+    }
+
     return (
         <div>
-            <h2>Please Add a new user</h2>
+            <h2>Add User</h2>
+            <form onSubmit={handleAddUser}>
+                <input onBlur={handleInputBlur} type='text' name='name' placeholder='name' required/>
+                <br/>
+                <input onBlur={handleInputBlur} type='text' name='address' placeholder='address' required/>
+                <br/>
+                <input onBlur={handleInputBlur} type='text' name='email' placeholder='email' required/>
+                <br/>
+                <button type='submit'>Add User</button>
+            </form>
         </div>
     );
 };
