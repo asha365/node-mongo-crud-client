7575515e47959ecabf54925759b7e87e070dadf8
unknown <asha.engrbd@gmail.com>
2023-03-22 03:22:36 +0000

    create update route. data load and show inside of form and changed

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Update = () => {
-     const user = useLoaderData()
+     const storedUser = useLoaderData();
+
+     const [user, setUser] = useState(storedUser);
+     const handleUpdateUser = event =>{
+     event.preventDefault();
+     console.log(user);
+ 
+     }
+ 
+     const handleInputChange = event =>{
+         const field = event.target.name;
+         const value = event.target.value;
+         const newUser = {...user}
+         newUser[field] = value;
+         setUser(newUser);
+     }
     return (
         <div>
-            <h2>Please Update: {user.name}</h2>
+            <h2>Please Update: {storedUser.name}</h2>
+            <form onSubmit={handleUpdateUser}>
+                <input onChange={handleInputChange} type='text' defaultValue={storedUser.name} name='name' placeholder='name' required/>
+                <br/>
+                <input onChange={handleInputChange} type='text' defaultValue={storedUser.address} name='address' placeholder='address' required/>
+                <br/>
+                <input onChange={handleInputChange} type='text' defaultValue={storedUser.email} name='email' placeholder='email' required/>
+                <br/>
+                <button type='submit'>Update User</button>
+            </form>
         </div>
     );
 };
